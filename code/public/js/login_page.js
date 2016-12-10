aaad8e58f4376aae98072e458f1b0ae3c018b1e5
Erik Landmark <erik.landmark@itggot.se>
2016-12-11 00:47:12 +0100

    Arbetade med login page.

--- code/public/js/login_page.js
+++ code/public/js/login_page.js
@@ -6,13 +6,34 @@ function openTab(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
+    // Get all elements with class="settingsTabItems" and hide them
+    tabcontent = document.getElementsByClassName("settingsContentItems");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" activeTab", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "flex";
+    evt.currentTarget.className += " activeTab";
+}
+
+function openPage(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="barcontent" and hide them
+    tabcontent = document.getElementsByClassName("barcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="barlinks" and remove the class "active"
     tablinks = document.getElementsByClassName("barlinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
@@ -23,7 +44,23 @@ function openTab(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
-function loadFiles(fileList) {
+
+function loadPublicFiles(fileList) {
+    files = fileList;
+
+    list = document.getElementById("publicFiles");
+
+    for (i = 0; i < files.length; i++) {
+        listItem = document.createElement('li');
+        list.appendChild(listItem);
+        dwnldLink = document.createElement('a');
+        dwnldLink.setAttribute("href", "/download/" + files[i].substring(files[i].lastIndexOf("/") + 1));
+        dwnldLink.textContent = files[i].substring(files[i].lastIndexOf("/") + 1);
+        listItem.appendChild(dwnldLink);
+    }
+}
+
+function loadPrivateFiles(fileList) {
     files = fileList;
 
     list = document.getElementById("privateFiles");
@@ -37,13 +74,3 @@ function loadFiles(fileList) {
         listItem.appendChild(dwnldLink);
     }
 }
-
-$(function () {
-    // 6 create an instance when the DOM is ready
-    $('#fileTree').jstree();
-    // 7 bind to events triggered on the tree
-    $('#fileTree').on("changed.fileTree", function (e, data) {
-        console.log(data.selected);
-
-    });
-});
